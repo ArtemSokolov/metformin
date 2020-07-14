3581edb61b3160af9260a61547aa915b709892a1
Artem Sokolov <artem.sokolov@gmail.com>
2020-07-14 15:57:58 -0400

    DFX plots/tables

--- dfx/plot.R
+++ dfx/plot.R
@@ -1,51 +1,56 @@
 library( tidyverse )
 
-load( "met-dfx.RData" )
+load( "all-dfx.RData" )
 
-## Identify the genes of interest
-DFX <- Rmet %>%
-    filter(FDR < 0.005,
-           sign(logFC40) == sign(logFC10),
-           abs(logFC40) > abs(logFC10))
+## Filter to top 80 dose-dependent perturbations
+DDX <- DFX %>%
+    map( filter, sign(logFC40) == sign(logFC10),
+        abs(logFC40) > abs(logFC10) ) %>%
+    map( arrange, FDR ) %>%
+    map( slice, 1:40 )
 
-## Isolate the corresponding slice of data
-Y <- Ymet %>% rownames_to_column( "Sample" ) %>% as_tibble()
-X <- Xmet %>% rownames_to_column( "Gene" ) %>% as_tibble()
-
-RR <- X %>% filter( Gene %in% DFX$Gene ) %>%
-    mutate( Med = pmap_dbl(list(S46, S47, S48), lift_vd(median)) ) %>%
-    gather( Sample, Counts, -Gene, -Med ) %>%
-    inner_join( Y, by="Sample" ) %>% 
-    mutate(logFC = log(Counts / Med),
-           logCnt = log10(Counts),
-           Sample = str_c(Sample, " ", Treatment))
-
-RX <- DFX %>% select( Gene, logFC10, logFC40 ) %>%
-    gather( Comparison, logFC, -Gene ) %>%
-    mutate_at( "Comparison", recode,
-              logFC10 = "Metformin (10uM)",
-              logFC40 = "Metformin (40uM)" )
+## Ceiling to the closest 0.1
+ceil.1 <- function( v ) {ceiling( v * 10 ) / 10}
 
 ## Plotting elements
-pal1 <- rev(RColorBrewer::brewer.pal(n=7, name="YlOrRd"))
-pal2 <- rev(RColorBrewer::brewer.pal(n=7, name="RdBu"))
+pal <- rev(RColorBrewer::brewer.pal(n=7, name="RdBu"))
+lmt <- 2
 
-gg1 <- ggplot( RR, aes(x=Sample, y=Gene, fill=logCnt) ) +
-    theme_minimal() + geom_tile() +
-    scale_fill_gradientn( colors=pal1, name="log10(Counts)" ) +
-    theme( axis.text.x = element_text( angle=90, vjust=0.5, hjust=1 ),
-          axis.text.y = element_text(size=9) )
+## Combine everything into a single data frame
+RX <- bind_rows( DDX, .id = "Treatment" ) %>%
+    mutate(logFCD = logFC40 - logFC10,
+           GeneID = str_c(Treatment, Gene)) %>%
+    arrange( logFCD ) %>%
+    mutate( GeneID = factor(GeneID, GeneID) ) %>%
+    select( Treatment, Gene, GeneID, logFC10, logFC40 ) %>%
+    gather( Comparison, logFCraw, logFC10, logFC40 ) %>%
+    mutate_at( "Comparison", recode,
+              logFC10 = "10uM vs DMSO",
+              logFC40 = "40uM vs DMSO" ) %>%
+    mutate_at( "Treatment", recode,
+              Gly24 = "Glyburide 24h",
+              Gly72 = "Glyburide 72h",
+              Met24 = "Metformin 24h",
+              Met72 = "Metformin 72h" ) %>%
+    mutate( logFC = case_when(logFCraw > lmt ~ lmt,
+                              logFCraw < -lmt ~ -lmt,
+                              TRUE ~ logFCraw),
+           lbl = str_c(round(logFCraw, 2)) )
 
-gg2 <- ggplot( RR, aes(x=Sample, y=Gene, fill=logFC) ) +
-    theme_minimal() + geom_tile() +
-    scale_fill_gradientn( colors=pal2, name="logFC", limits=c(-1.2, 1.2) ) +
-    theme( axis.text.x = element_text( angle=90, vjust=0.5, hjust=1 ) )
+flbl <- function( v ) {with(RX, set_names(Gene, GeneID))[v]}
 
-gg3 <- ggplot( RX, aes(x=Comparison, y=Gene, fill=logFC) ) +
+## Plot as a faceted heatmap
+gg <- ggplot( RX, aes(x=Comparison, y=GeneID, fill=logFC) ) +
     theme_minimal() + geom_tile() +
-    scale_fill_gradientn( colors=pal2, name="logFC", limits=c(-0.85, 0.85) ) +
-    theme( axis.text.x = element_text( angle=90, vjust=0.5, hjust=1 ) )
-    
-gg <- cowplot::plot_grid( gg1, gg2, gg3, ncol=3, rel_widths=c(1.1,1,0.7),
-                         labels=str_c("Demo", 1:3), label_x=c(0.6, 0.7, 0.4) )
-ggsave( "test.pdf", gg, width=9, height=10 )
+    geom_text( aes(label=lbl), data=filter(RX, abs(logFCraw) > 2), color="gold" ) +
+    facet_wrap( ~Treatment, ncol=4, scales="free_y" ) +
+    scale_y_discrete( labels = flbl, name="Gene" ) +
+    scale_fill_gradientn( colors=pal, name="logFC", limits=c(-lmt, lmt) ) +
+    theme(axis.text.x = element_text( angle=90, vjust=0.5, hjust=1 ),
+          strip.text  = element_text(size=12),
+          panel.grid  = element_blank() )
+
+ggsave( "FigA.pdf", gg, width=9, height=7 )
+
+## Save all differential gene expression as a supplementary table
+openxlsx::write.xlsx( file="Suppl-Table-A.xlsx" )
